0c0dd63cdcaa5a443c1cbc2dd99c58289b23e192
Nandakumar9 <nandakumar.09.1991@gmail.com>
2021-11-14 15:21:28 +0530

    angular changes

---
+++ testAngular/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { SuccessComponent } from './success/success.component';
+import { WarningComponent } from './warningalert/warningalert.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SuccessComponent,
+    WarningComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
